c89ba130ee479f8b10aa790bf6bcab533ffccbb3
evribsap <evribsaputra@gmail.com>
2022-08-03 21:03:51 +0700

    update navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -27,7 +27,7 @@ const Navbar = () => {
       <a href="/" className="p__opensans">Book Table</a>
     </div>
     <div className="app__navbar-smallscreen">
-      <GiHamburgerMenu color="#fff" fontSize={27} onClick={() => setToggleMenu(true)} />
+      <GiHamburgerMenu color="#fff" fontSize={27} className="overlay__open" onClick={() => setToggleMenu(true)} />
 
       {toggleMenu && (
       <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
