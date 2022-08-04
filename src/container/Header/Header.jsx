84fb9acecb21f0d950205ca1b3e9d26badf854a6
evribsap <evribsaputra@gmail.com>
2022-08-04 22:18:14 +0700

    add about us

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -14,7 +14,7 @@ const Header = () => (
       <p className="p__opensans" style={{ margin: '2rem 0' }}>
         Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet tellus 
       </p>
-      <button className="custom__button">Expolore Menu</button>
+      <button type="button" className="custom__button">Expolore Menu</button>
     </div>
     <div className="app__wrapper_img">
       <img src={images.welcome} alt="header img" />
